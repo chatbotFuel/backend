6d6cbdd8ca2cd1858cfa85742da8280475bd0c0d
Mouther <za7788az@hotmail.com>
2017-10-11 23:05:09 +0800

    add booking

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const jsonfile = require("jsonfile")
 const mysql = require("mysql")
 const randomstring = require("randomstring")
+const bodyParser = require('body-parser')
 
 const config = jsonfile.readFileSync(__dirname + "/config.json")
 const db     = jsonfile.readFileSync(__dirname + "/db.json")
@@ -13,11 +14,16 @@ let app = express()
 let pool = mysql.createPool(db)
 
 app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/chat-redirect', (req, res) => {
   let token = randomstring.generate(randomConfig)
   pool.query("INSERT INTO token (token, name, id) VALUES (?, ?, ?)", [token, req.query["last name"] + req.query["first name"], req.query["messenger user id"]], (err, result, field) => {
-    if(err) console.log(err)
+    if (err) {
+      console.log(err)
+      return res.send("error")
+    }
     res.send({
       "messages": [
     	  {
@@ -29,7 +35,7 @@ app.get('/chat-redirect', (req, res) => {
     	        "buttons": [
     	          {
     	            "type": "web_url",
-    	            "url": config.host + ":" + config.port,
+    	            "url": config.outerHost + "?token=" + token,
     	            "title": "Visit Website"
     	          },
     	        ]
@@ -41,13 +47,21 @@ app.get('/chat-redirect', (req, res) => {
   })
 })
 
-app.get('/get_list', (req, res) => {
+app.post('/booking', (req, res) => {
+  let token = req.body.token
+  pool.query("SELECT * FROM token WHERE token.token = ?", [token], (err, result, field) => {
+    if (result.length == 0) return res.send("Invalid token")
+    res.send(result)
+  })
+})
+
+app.get('/get-list', (req, res) => {
   pool.query("SELECT * FROM list", (err, result, field) => {
     res.send(result)
   })
 })
 
-app.get('/get_restaurant_book_info', (req, res) => {
+app.get('/get-restaurant-book-info', (req, res) => {
   pool.query("SELECT * FROM book_info INNER JOIN list ON book_info.id = list.id WHERE list.name = ?", [req.query.restaurant_name], (err, result, field) => {
     res.send(result)
   })
